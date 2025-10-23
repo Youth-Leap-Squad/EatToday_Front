ea93513aedcb98e2d81850163618c0868afbdd6d
pilltong22 <rlawlsgh6@naver.com>
2025-10-23 18:26:43 +0900

    프론트 폴더 구조 생성(components 안에 있는 폴더는 각자 알아서 추가할것 추가하기

---
+++ eatToday_front/src/App.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+  <footer class="footer">
+    <div class="footer-wrap">
+      <a class="brand" href="/">
+          <img src="@/assets/images/logo.png" alt="오늘 뭐랑?" />
+          <span>오늘 뭐랑?</span>
+      </a>
+
+
+
+
+
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+
+</style>

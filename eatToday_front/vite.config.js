d894e04475d40e29b0c1c5abbaed3351330b011f
woo-kyoung-nam <dnrud6911@gmail.com>
2025-10-28 19:48:17 +0900

    event

--- eatToday_front/vite.config.js
+++ eatToday_front/vite.config.js
@@ -58,10 +58,6 @@ export default defineConfig({
         target: 'http://localhost:8080',
         changeOrigin: true
       },
-      '/images': {
-        target: 'http://localhost:8080', 
-        changeOrigin: true,
-      },
     },
   },
 })
